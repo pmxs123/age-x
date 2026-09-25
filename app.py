e6444dfbf51776cca6c8fe7ee772a75195617e2c
pmxs123 <3254095714@qq.com>
2026-09-25 17:19:33 +0800

    Update app.py

--- app.py
+++ app.py
@@ -6,12 +6,14 @@ st.subheader("入场资格核验")
 
 # 输入框
 wa = st.text_input("请输入你的姓名：")
-asb = st.number_input("请输入你的年龄：", min_value=0, max_value=120, value=18)
+asb = st.number_input("请输入你的年龄：", min_value=0, max_value=120, value=None)
 vp = st.number_input("你的VIP等级是多少？：", min_value=0, max_value=999, value=0)
 
 # 判断逻辑
 if st.button("提交核验"):
-    if asb > 18:
+    if asb is None:
+        st.warning("请填写年龄！")
+    elif asb > 18:
         st.success(f"{wa}，已成年允许进入")
         if vp > 50:
             st.info("你的vip已达到50级，享有专属服务😘")
